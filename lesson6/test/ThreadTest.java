ec490d840b175225b37b6c32469a3c3420f1764e
cuc9 <cuc91111@gmail.com>
2016-09-13 21:40:41 +0300

    6

---
+++ lesson6/test/ThreadTest.java
@@ -0,0 +1,17 @@
+package lesson6.test;
+
+/**
+ * Created by arpi on 16.04.2016.
+ */
+public class ThreadTest extends Thread {
+
+
+    @Override
+    public void run() {
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
