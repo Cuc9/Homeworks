bdb83824980750862dd59ce327af45defa4b13a0
cuc9 <cuc91111@gmail.com>
2016-09-13 21:42:59 +0300

    2

---
+++ lesson2/Figures/Figure.java
@@ -0,0 +1,19 @@
+package lesson2.Figures;
+
+/**
+ * Created by arpi on 02.02.2016.
+ */
+public abstract class Figure {
+    private String name;
+
+    public void setNmae (String name) {
+        this.name = name;
+    }
+
+    public String getName () {
+        return this.name;
+    }
+
+    public abstract double square ();
+
+}
