9ad7efcb9e170ae3f29a97efaaf3e73d34ab7156
cuc9 <cuc91111@gmail.com>
2016-09-13 21:43:38 +0300

    3

---
+++ lesson3/RectanglePrinter/MainRect.java
@@ -0,0 +1,11 @@
+package lesson3.RectanglePrinter;
+
+/**
+ * Created by arpi on 08.02.2016.
+ */
+public class MainRect {
+    public static void main(String[] args) {
+        Rectangle r = new Rectangle();
+        r.printRectangle();
+    }
+}
