9ed56c51b987609ac96614262545ab7dcb4b5520
cuc9 <cuc91111@gmail.com>
2016-09-13 21:43:05 +0300

    2

---
+++ lesson2/Figures/Rectangle.java
@@ -0,0 +1,36 @@
+package lesson2.Figures;
+
+/**
+ * Created by arpi on 02.02.2016.
+ */
+public class Rectangle extends Figure {
+
+    private double length;
+    private double height;
+
+    public double getLength() {
+        return length;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setLength(double length) {
+        this.length = length;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public Rectangle(double length, double height) {
+        this.length = length;
+        this.height = height;
+    }
+
+    @Override
+    public double square() {
+        return length * height;
+    }
+}
