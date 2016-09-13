8894d8363a4adc485f023404ef2ee5ada4274d0e
cuc9 <cuc91111@gmail.com>
2016-09-13 21:37:15 +0300

    12
    12

---
+++ lesson12/chat/Message.java
@@ -0,0 +1,84 @@
+package lesson12.chat;
+
+import java.io.*;
+import java.util.Date;
+
+public class Message implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	public Date date = new Date();
+	public String from;
+	public String to;
+	public boolean isFile;
+	public transient String text;
+	public transient String path;
+	
+	@Override
+	public String toString() {
+		return new StringBuilder()
+			.append("[")
+			.append(date.toString())
+			.append(", From: ")
+			.append(from)
+			.append(", To: ")
+			.append(to)
+			.append("] ")
+			.append(text)
+			.toString();
+	}
+	
+	public void writeToStream(OutputStream out)
+		throws IOException
+	{
+		ByteArrayOutputStream bs = new ByteArrayOutputStream();
+		ObjectOutputStream os = new ObjectOutputStream(bs);
+		try {
+			os.writeObject(this);
+			
+			if ( ! isFile) {
+				os.writeUTF(text);
+			} else {
+				// write file content
+			}
+		} finally {
+			os.flush();
+			os.close();
+		}
+		
+		byte[] packet = bs.toByteArray();
+		
+		DataOutputStream ds = new DataOutputStream(out);
+		//System.out.println("I write " + packet.length + " -- " + packet);  ////
+		ds.writeInt(packet.length);
+		ds.write(packet);
+		ds.flush();
+	}
+	
+	public static Message readFromStream(InputStream in)
+		throws IOException, ClassNotFoundException
+	{
+		if (in.available() <= 0)
+			return null;
+		
+		DataInputStream ds = new DataInputStream(in);
+		int len = ds.readInt();
+		byte[] packet = new byte[len];
+		ds.read(packet);
+		
+		ByteArrayInputStream bs = new ByteArrayInputStream(packet);
+		ObjectInputStream os = new ObjectInputStream(bs);
+		try {
+			Message msg = (Message) os.readObject();
+			if ( ! msg.isFile) {
+				msg.text = (String) os.readUTF();
+			//	System.out.println("I obtained " + msg.text);
+			} else {
+				// read file content
+			}
+			
+			return msg;
+		} finally {
+			os.close();
+		}
+	}
+}
---
+++ lesson12/chatServer/Main.java
@@ -0,0 +1,11 @@
+package lesson12.chatServer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Server s = new Server(5000);
+        s.start();
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            s.stop();
+        }));
+    }
+}
