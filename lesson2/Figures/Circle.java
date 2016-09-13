b3278d9d1ea95c68e64a344a2b77711325fc4acd
cuc9 <cuc91111@gmail.com>
2016-09-13 21:42:55 +0300

    1

---
+++ lesson2/Figures/Circle.java
@@ -0,0 +1,26 @@
+package lesson2.Figures;
+
+/**
+ * Created by arpi on 02.02.2016.
+ */
+public class Circle extends Figure {
+
+    private double radius;
+
+    public Circle (double radius) {
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    private void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public double square (){
+        return Math.PI*Math.pow(radius,2);
+    }
+}
